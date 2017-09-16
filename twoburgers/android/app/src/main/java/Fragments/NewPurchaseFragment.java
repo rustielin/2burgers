6474955b5213f8f8bcc58dbf12711a812ee8da17
rustielin <rustielin@berkeley.edu>
2017-09-16 03:51:56 -0700

    bottom burger

--- twoburgers/android/app/src/main/java/Fragments/NewPurchaseFragment.java
+++ twoburgers/android/app/src/main/java/Fragments/NewPurchaseFragment.java
@@ -1,13 +1,20 @@
 package Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatSpinner;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.rustie.twoburgers.R;
+
+import Activities.MainActivity;
 
 /**
  * Created by rustie on 9/15/17.
@@ -16,6 +23,10 @@ import com.example.rustie.twoburgers.R;
 public class NewPurchaseFragment extends Fragment {
 
     private View v;
+
+    private AppCompatSpinner mSpinner;
+    private EditText mPrice;
+    private AppCompatButton mSubmit;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -27,6 +38,22 @@ public class NewPurchaseFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         v = getActivity().getLayoutInflater().inflate(R.layout.new_purchase_fragment_layout, null);
+
+        mSpinner = (AppCompatSpinner) v.findViewById(R.id.category_spinner);
+        mPrice = (EditText) v.findViewById(R.id.price);
+        mSubmit = (AppCompatButton) v.findViewById(R.id.submit);
+
+        mSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
+
+
 
         return v;
     }
